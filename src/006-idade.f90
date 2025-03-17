4a9a2313391f1c16339b9af71923612fba0ac29b
Romulo Beninca <rbeninca@gmail.com>
2025-03-17 11:34:34 -0300

    correação calculo da idade, levando em consideração o dia do mês  IF MESNASCIMENTO>MESATUAL  .OR. MESNASCIMENTO==MESATUAL E DIANASCIMENTO<DIAATUAL.

--- src/006-idade.f90
+++ src/006-idade.f90
@@ -1,6 +1,6 @@
 program calcula_idade
 
-    implicit none
+    IMPLICIT NONE
 
     INTEGER :: data_nascimento(3),data_atual(8),idade;
 
@@ -9,18 +9,18 @@ program calcula_idade
     READ (*,'(I2,1X,I2,1X, I4)') data_nascimento(1), data_nascimento(2), data_nascimento(3) 
     !READ (*,*) data_nascimento(1), data_nascimento(2), data_nascimento(3)
 
-    call date_and_time(values=data_atual)
+    CALL DATE_AND_TIME(values=data_atual)
     idade = data_atual(1) - data_nascimento(3)
 
-    if (data_nascimento(2) > data_atual(2)) then
+    IF  ( (data_nascimento(2) > data_atual(2)) .or.( (data_nascimento(2) == data_atual(2)) .and. (data_nascimento(1) > data_atual(3)) ) ) THEN
         idade = idade - 1
-    else if (data_nascimento(2) == data_atual(2)) then
-        if (data_nascimento(1) > data_atual(1)) then
+    ELSE IF (data_nascimento(2) == data_atual(2)) THEN
+        IF (data_nascimento(1) > data_atual(1)) THEN
             idade = idade - 1
-        end if
-    end if
+        END IF
+    END IF
     WRITE (*, '(A,I2.2,A,I2.2,A,I4.4)') "Você nasceu em:",&
                data_nascimento(1), "/", data_nascimento(2), "/", data_nascimento(3)
-    print *, "Você tem ", idade, " anos de idade."
+    PRINT  *, "Você tem ", idade, " anos de idade."
 
-end program calcula_idade
+END PROGRAM calcula_idade
