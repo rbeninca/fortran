a32454beb3fefe8cbebad31ed8e5fe15891106b5
Romulo Beninca <rbeninca@gmail.com>
2025-04-09 00:09:41 -0300

    auto check win linux  com problema devido falta de variaveis de ambientes win powerstation

--- src/Exercícios/avaliacao/prova1.f90
+++ src/Exercícios/avaliacao/prova1.f90
@@ -5,6 +5,7 @@ PROGRAM prova1
   ! VARIÁVEIS
   CHARACTER(LEN=50) :: Aluno
   CHARACTER(LEN=50) :: Dia
+  INTEGER :: os
   INTEGER :: L1, L2, L3
   REAL :: M1, M2, M3, M4, M5, pi, base, numerador, denominador
 
@@ -13,13 +14,19 @@ PROGRAM prova1
 
   ! 3) Leitura dos dados
   WRITE(*,*) 'Digite o nome completo do aluno:'
-  READ(*,'(A)') Aluno
+  !READ(*,'(A)') Aluno
 
   WRITE(*,*) 'Digite a data de hoje (dia, mês por extenso e ano com 4 algarismos):'
-  READ(*,'(A)') Dia
+  !READ(*,'(A)') Dia
 
   WRITE(*,*) 'Digite os valores de L1, L2 e L3:'
-  READ(*,*) L1, L2, L3
+  !READ(*,*) L1, L2, L3
+  !Leitura dos dados teste
+  Aluno = "Romulo de Aguiar Beninca"
+  Dia = "08 de Abril de 2025"
+  L1 = 4
+  L2 = 2
+  L3 = 3
 
   ! 4) Escrever Aluno, Dia, L1, L2 e L3
   WRITE(10,'(A,1X,A)') 'Aluno =', TRIM(Aluno)
@@ -55,8 +62,18 @@ PROGRAM prova1
   WRITE(10,'(A,1X,ES13.3E2)') 'M5 =', M5
   
   CLOSE (10)
-   ! Abrir o arquivo de saída no Notepad (Windows)
-  CALL SYSTEM('start notepad out1.txt')
+
+CALL SYSTEM('uname > /dev/null', os)
+      IF (os .EQ. 0) THEN
+         PRINT *, 'Sistema operacional: Linux ou Unix'
+         CALL SYSTEM('xdg-open out1.txt')
+      ELSE
+         PRINT *, 'Provavelmente Windows'
+         CALL SYSTEM('start notepad out1.txt',os)
+      ENDIF
+
+
+  
 
 
 END PROGRAM prova1
