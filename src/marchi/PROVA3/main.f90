6a9c6fda4849442e81d09c6a6e15ad64b0f47ddc
Romulo de Aguiar Beninca <rbeninca@gmail.com>
2025-05-29 20:21:28 -0300

    corridingo saidas fiel out ainda vazio

--- src/marchi/PROVA3/main.f90
+++ src/marchi/PROVA3/main.f90
@@ -5,10 +5,53 @@ implicit none
 character(len=100) :: nome, dia
 real*8 :: K1, d, N, L, X, M1, M2, M3, M4, M5, M6, M7
 integer :: b, aux
-open(20, file='OUT3.TXT',STATUS='UNKNOWN',ACTION='WRITE')
+
 call DADOS(nome, dia, K1, d, N, L, X)
 
+ ! Impressão para conferência na consola
+  write(*,100) Nome
+  write(*,101) Dia
+  write(*,102) K1
+  write(*,103) d
+  write(*,104) N  ! Corrigido para usar o formato 104
+  write(*,105) L  ! Corrigido para usar o formato 105
+  write(*,106) X  ! Corrigido para usar o formato 106
+
+
+
+! (b) M1 = exp(K1)
+M1 = dexp(K1)
+! (c) M2 = log(d)
+M2 = dlog(d)
+! (d) M3 = cosh(N)
+M3 = dcosh(N)
+! (e) Calcular M4
+M4 = 0.d0
+do b = 0, int(d)
+  M4 = M4 + (4.d0*(b-1)*(2*b+1))/(2*b+1)
+end do
+
+
+! (f) Calcular M5
+M5 = 0.d0
+do b = 0, int(d), 2
+  M5 = M5 + 5.d0/factorial(b)
+end do
+! (g) Calcular M6 e M7
+call SERIES(int(d), int(X), M6, M7)
+close(20)
+
+open(9, file='OUT3.TXT',STATUS='UNKNOWN',ACTION='WRITE')
+ 
  ! Escrita dos dados de entrada no ficheiro
+  write(9,100) Nome
+  write(9,101) Dia
+  write(9,102) K1
+  write(9,103) d
+  write(9,104) N
+  write(9,105) L
+  write(9,106) 20
+
   write(9,100) Nome
   write(9,101) Dia
   write(9,102) K1
@@ -17,41 +60,7 @@ call DADOS(nome, dia, K1, d, N, L, X)
   write(9,105) L
   write(9,106) X
 
-
-! (b) M1 = exp(K1)
-M1 = dexp(K1)
-write(20,'(E11.9)') M1 ! M1
-
-! (c) M2 = log(d)
-M2 = dlog(d)
-write(20,'(E11.9)') M2 ! M2
-
-! (d) M3 = cosh(N)
-M3 = dcosh(N)
-write(20,'(E11.9)') M3 ! M3
-
-! (e) Calcular M4
-M4 = 0.d0
-do b = 0, int(d)
-  M4 = M4 + (4.d0*(b-1)*(2*b+1))/(2*b+1)
-end do
-write(20,'(E11.9)') M4 ! M4
-
-! (f) Calcular M5
-M5 = 0.d0
-do b = 0, int(d), 2
-  M5 = M5 + 5.d0/factorial(b)
-end do
-write(20,'(E11.9)') M5 ! M5
-
-! (g) Calcular M6 e M7
-call SERIES(int(d), int(X), M6, M7)
-write(20,'(E11.9)') M6 ! M6
-write(20,'(E11.9)') M7 ! M7
-close(20)
 CALL SYSTEM("start notepad OUT3.TXT")
-
-
 
 ! FORMATs obrigatórios
 100 format('Nome = ', A50)
@@ -61,5 +70,14 @@ CALL SYSTEM("start notepad OUT3.TXT")
 104 format('N = ', 1PE20.10)  ! Formato para N
 105 format('L = ', 1PE20.10)  ! Formato para L
 106 format('X = ', I10)      ! Formato para X
+110 format('M1 = ', 1PE20.10)
+111 format('M2 = ', 1PE20.10)
+112 format('M3 = ', 1PE20.10)
+113 format('M4 = ', 1PE20.10)
+114 format('M5 = ', 1PE20.10)
+115 format('M6 = ', 1PE20.10)
+116 format('M7 = ', 1PE20.10)
+
 end program PROVA3
+
 									   
