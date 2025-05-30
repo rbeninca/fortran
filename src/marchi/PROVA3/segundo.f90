7cd0601dd14c99a74a46e839380e5c2f6a73aef4
Romulo de Aguiar Beninca <rbeninca@gmail.com>
2025-05-29 21:14:12 -0300

    rotinas

--- src/marchi/PROVA3/segundo.f90
+++ src/marchi/PROVA3/segundo.f90
@@ -1,16 +1,16 @@
 module ROTINAS
+   use portlib
    implicit none
-
 contains
 
-   subroutine  DADOS(Nome, Dia, K1, d, N, L, X)
+   subroutine DADOS(Nome, Dia, K1, d, N, L, X)
       character(len=50), intent(out) :: Nome, Dia
       real*8, intent(out) :: K1, N, L
       integer, intent(out) :: d, X
 
-      open(8, file='ENTRA3.TXT', status='old')
-      read(8, '(A)') Nome
-      read(8, '(A)') Dia
+      open(8, file="ENTRA3.TXT")
+      read(8,'(A)') Nome
+      read(8,'(A)') Dia
       read(8,*) K1
       read(8,*) d
       read(8,*)  N
@@ -20,23 +20,25 @@ contains
       close(8)
    end subroutine DADOS
 
-   subroutine SERIES(d, X, M6, M7)
-      integer, intent(in) :: d, X
-      real*8, intent(out) :: M6, M7
-      integer :: b
-      real*8 :: termo, fat
-      M6 = 0.0
-      M7 = 0.0
+subroutine SERIES(d, X, M6, M7)
+    integer, intent(in) :: d, X
+    real*8, intent(out) :: M6, M7
+    integer :: b
+    real*8 :: termo, fat
 
-      do b = 1, d
-         M6 = M6 + 1.d0/((b*(b+1)*(b+2)))
-      end do
+    M6 = 0.d0
+    do b = 1, d
+		termo = ((1.0d0) / ( b*(b+1)*(b+2) ))
+      M6 = M6 +  termo
+    end do
 
-      do b = 0, X
-         call fatorial(2*b + 1, fat)
-         M7 = M7 + ((-1.d0)**(b))*(2*b + 1)/fat
-      end do
-   end subroutine SERIES
+    M7 = 0.d0
+    do b = 0, X, 1
+      call fatorial(2*b + 1, fat)
+      termo = ((-1)**b * (0.5)**(2*b + 1)) / fat
+      M7 = M7 + termo
+    end do
+  end subroutine SERIES
 
    subroutine fatorial(j, fat)
       integer, intent(in) :: j
